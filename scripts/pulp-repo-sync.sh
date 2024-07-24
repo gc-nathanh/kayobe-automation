5a2bb9f9fa3959f7e7aecc6ad125e0c459f619e8
Nathan Harper <nathanh@graphcore.ai>
2024-07-24 15:26:09 +0100

    correct path

--- scripts/pulp-repo-sync.sh
+++ scripts/pulp-repo-sync.sh
@@ -9,7 +9,7 @@ source "${PARENT}/../functions"
 
 function main {
     kayobe_init
-    run_kayobe playbook run pulp-repo-sync.yml "${@}"
+    run_kayobe playbook run ansible/pulp-repo-sync.yml "${@}"
     pull_request "${KAYOBE_AUTOMATION_CONTEXT_ENV_PATH}/src/kayobe-config"
 }
 
