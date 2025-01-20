e6f89d0290dd68a323f44214010bfbb8f6e56431
Nathan Harper <nathanh@graphcore.ai>
2025-01-20 10:23:07 +0000

    more verbosity

--- pipeline/config-diff.sh
+++ pipeline/config-diff.sh
@@ -136,7 +136,8 @@ function generate_config {
     export KAYOBE_VAULT_PASSWORD_OLD="$KAYOBE_VAULT_PASSWORD"
     export KAYOBE_VAULT_PASSWORD=dummy-password
     local KAYOBE_ANSIBLE_PATH="$env_path/venvs/kayobe/share/kayobe/ansible"
-    kayobe control host bootstrap
+    kayobe control host bootstrap -vvvv --diff
+
     log_info "Generating config to $output_dir"
     kayobe playbook run "$KAYOBE_ANSIBLE_PATH/kayobe-automation-prepare-config-diff.yml"
     kolla_ansible_cfg=$env_path/src/kayobe-config/etc/kayobe/kolla/ansible.cfg
